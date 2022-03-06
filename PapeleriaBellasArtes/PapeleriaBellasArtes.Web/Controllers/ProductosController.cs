76d9ecbbb793f9c5a2b3e53898702370cd1e98ad
Cyinthia <Cyinthia@DESKTOP-N8ID8VL>
2022-03-05 21:20:20 -0600

    Actualizando

---
+++ PapeleriaBellasArtes/PapeleriaBellasArtes.Web/Controllers/ProductosController.cs
@@ -0,0 +1,21 @@
+﻿using PapeleriaBellasArtes.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PapeleriaBellasArtes.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
+
+            return View(listadeProductos);
+        }
+    }
+}
