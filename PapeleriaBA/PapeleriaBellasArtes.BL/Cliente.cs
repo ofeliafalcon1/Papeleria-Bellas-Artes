46d6dc990ff4785ec372a0b7243089f9d52c8442
Cyinthia <Cyinthia@DESKTOP-N8ID8VL>
2022-03-17 02:46:28 -0600

    Avance Tarea #5:  Agregando los controladores, ClasesBL para las órdenes.

---
+++ PapeleriaBA/PapeleriaBellasArtes.BL/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapeleriaBellasArtes.BL
+{
+    public class Cliente
+    {
+        [Required(ErrorMessage ="Ingrese el nombre del cliente.")]
+        [MinLength(3, ErrorMessage ="Ingrese mínimo 3 carácteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage ="Ingrese el teléfono por favor.")]
+        [MinLength(8, ErrorMessage = "El teléfono debe ser de 8 digitos.")]
+        [MaxLength(8, ErrorMessage = "El teléfono debe ser de 8 digitos.")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "Ingrese la direccion")]
+        [MinLength(3, ErrorMessage = "Ingrese mínimo 3 caracteres")]
+        public string Direccion { get; set; }
+        public bool Activo { get; set; }
+    }
+}
--- PapeleriaBA/PapeleriaBellasArtes.BL/Contexto.cs
+++ PapeleriaBA/PapeleriaBellasArtes.BL/Contexto.cs
@@ -24,5 +24,10 @@ namespace PapeleriaBellasArtes.BL
 
         public DbSet<Producto> Productos { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Orden> Ordenes { get; set; }
+        public DbSet<OrdenDetalle> OrdenDetalle { get; set; }
+
     }
 }
