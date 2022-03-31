93449a4f9fe1ea9a123f441e35fd926f156ddf49
DELL <DELL@JAVIERFV>
2022-03-31 01:12:09 -0600

    Actualizacion tarea #7:  Agregando la plantilla a nuestro Sitio Web público.

---
+++ PapeleriaBellasArtes/PapeleriaBellasArtes.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using PapeleriaBellasArtes.BL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PapeleriaBellasArtes.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
+            ViewBag.adminWebSiteUrl = ConfigurationManager.AppSettings["adminWebSiteUrl"];
+            return View(listadeProductos);
+        }
+    }
+}
